a0b3cd58533140b024fa84648fa28be9c9634c05
Too large to fit in the margin <qgbcs@outlook.com>
2026-07-21 04:26:54 +0000

    官方补丁，加入numpy cv2 95M大小 hualing-0.1-arm64-v8a-debug-20260721_042332.apk

--- android_src/numpy/__init__.py
+++ android_src/numpy/__init__.py
@@ -1,19 +1,68 @@
-from pythonforandroid.recipe import Recipe
+from pythonforandroid.recipe import Recipe, MesonRecipe
 from os.path import join
+import shutil
 
-class NumpyRecipe(Recipe):
-    version = 'v1.26.4'     # 直接带 v 的 git tag
-    url = 'git+https://github.com/numpy/numpy'
-    patches = []            # 不用补丁
+NUMPY_NDK_MESSAGE = (
+    "In order to build numpy, you must set minimum ndk api (minapi) to `24`.\n"
+)
 
-    def get_recipe_env(self, arch=None):
-        env = super().get_recipe_env(arch)
-        # 只在交叉编译阶段加入修复标志
-        env['CXXFLAGS'] = env.get('CXXFLAGS', '') + ' -std=c++17 -include unordered_map'
-        env['CFLAGS'] = env.get('CFLAGS', '') + ' -include unordered_map'
+
+class NumpyRecipe(MesonRecipe):
+    version = "v2.3.0"
+    url = "git+https://github.com/numpy/numpy"
+    extra_build_args = ["-Csetup-args=-Dblas=none", "-Csetup-args=-Dlapack=none"]
+    opt_depends = ["libopenblas"]
+    need_stl_shared = True
+    min_ndk_api_support = 24
+
+    def get_include(self, arch):
+        return join(
+            self.ctx.get_python_install_dir(arch.arch), "numpy/_core/include",
+        )
+
+    def get_recipe_meson_options(self, arch):
+        options = super().get_recipe_meson_options(arch)
+        options["properties"]["longdouble_format"] = (
+            "IEEE_DOUBLE_LE" if arch.arch in ["armeabi-v7a", "x86"] else "IEEE_QUAD_LE"
+        )
+        return options
+
+    def get_recipe_env(self, arch, **kwargs):
+        env = super().get_recipe_env(arch, **kwargs)
+
+        # _PYTHON_HOST_PLATFORM declares that we're cross-compiling
+        # and avoids issues when building on macOS for Android targets.
+        env["_PYTHON_HOST_PLATFORM"] = arch.command_prefix
+
+        # NPY_DISABLE_SVML=1 allows numpy to build for non-AVX512 CPUs
+        # See: https://github.com/numpy/numpy/issues/21196
+        env["NPY_DISABLE_SVML"] = "1"
+        env["TARGET_PYTHON_EXE"] = join(
+            Recipe.get_recipe("python3", self.ctx).get_build_dir(arch.arch),
+            "android-build",
+            "python",
+        )
+        blas_dir = join(Recipe.get_recipe("libopenblas", self.ctx
+        ).get_build_dir(arch.arch), "build")
+        blas_incdir = blas_dir
+        blas_libdir = join(blas_dir, "lib")
+        env["CXXFLAGS"] += f" -I{blas_incdir} -L{blas_libdir}"
+
+        if 'libopenblas' in self.ctx.recipe_build_order:
+            self.extra_build_args = [
+                "-Csetup-args=-Dblas=auto",
+                "-Csetup-args=-Dlapack=auto",
+                "-Csetup-args=-Dallow-noblas=False",
+            ]
+
         return env
+
+    def get_hostrecipe_env(self, arch=None):
+        env = super().get_hostrecipe_env(arch=arch)
+        env["RANLIB"] = shutil.which("ranlib")
+        return env
+
 
 recipe = NumpyRecipe()
 
-#mkdir android_src/numpy
-#touch android_src/numpy/__init__.py
+#wget https://raw.githubusercontent.com/kivy/python-for-android/develop/pythonforandroid/recipes/numpy/__init__.py -O android_src/numpy/__init__.py
